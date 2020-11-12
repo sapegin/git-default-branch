cd9a72132713f9a4c9d45d853ad4479bb105b156
Artem Sapegin <artem@sapegin.ru>
2020-11-12 08:43:01 +0100

    feat: Check Git config file and ask Git if a branch exists

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const fs = require('fs');
 const path = require('path');
+const execSync = require('child_process').execSync;
 
 const getDefaultValue = () => {
 	const filename = path.resolve(process.cwd(), '.git/config');
@@ -10,15 +11,33 @@ const getDefaultValue = () => {
 	}
 };
 
+const branchExists = (branch) => {
+	try {
+		execSync(`git show-ref --verify --quiet refs/heads/${branch}`);
+		return true;
+	} catch (err) {
+		return false;
+	}
+};
+
 module.exports = (config = getDefaultValue()) => {
 	if (!config) {
 		return '';
 	}
 
+	// Check Git config file first, it's faster
 	if (config.includes('[branch "main"]')) {
 		return 'main';
 	}
 	if (config.includes('[branch "master"]')) {
+		return 'master';
+	}
+
+	// Ask Git
+	if (branchExists('main')) {
+		return 'main';
+	}
+	if (branchExists('master')) {
 		return 'master';
 	}
 
